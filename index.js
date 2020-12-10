e50124d40f63447fda3700bbda267a5ba2182ef5
Andrei Stefan Bitca <andrei.bitca@deepcrawl.com>
2020-12-10 11:44:41 +0000

    update

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-const core = require('@actions/core');
-const axios = require('axios');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const axios = require("axios");
 
 const URL = "https://ht9ztjsdg5.execute-api.us-east-1.amazonaws.com/prod/trigger-build";
 
 async function run() {
-  const userKeyId = core.getInput('userKeyId');
-  const userKeySecret = core.getInput('userKeySecret');
-  const testSuiteId = core.getInput('testSuiteId');
+  const userKeyId = core.getInput("userKeyId");
+  const userKeySecret = core.getInput("userKeySecret");
+  const testSuiteId = core.getInput("testSuiteId");
+  const { sha, repo: { owner: repoOwner, repo: repoName } } = github.context;
   const response = await axios.post(URL, {
     userKeyId,
     userKeySecret,
     testSuiteId,
+    repoOwner,
+    repoName,
+    sha,
   });
   core.setOutput("buildId", JSON.parse(response.data).buildId);
 }
